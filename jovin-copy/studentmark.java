9fbcf9e90531230e528eaf01521481ff7ce0cb94
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-07-17 20:43:28 +1000

    created repository
    created a new file , started the coding and commenting

---
+++ jovin-copy/studentmark.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class studentmark here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.scanner; // this is done for allowing the code to fetch the input from the user
+
+public class studentmark
+{
+ public static void main (String[] args) //shows the code where to start
+ scanner takeinput = new Scanner(System.in);
+ double[] marks = new double[30]; //here we create a marks array of length 30 which is mentioned in the question
+}
