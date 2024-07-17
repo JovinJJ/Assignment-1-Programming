e19ceb4c14fac91252f1b6ebcdbb883280e810e9
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-07-17 21:33:36 +1000

    corrected errors

--- jovin-copy/studentmark.java
+++ jovin-copy/studentmark.java
@@ -6,20 +6,24 @@
  * @version (a version number or a date)
  */
 
-import java.util.scanner; // this is done for allowing the code to fetch the input from the user
+import java.util.Scanner; // this is done for allowing the code to fetch the input from the user
 
 public class studentmark
 {
  
- scanner takeinput = new Scanner(System.in);
- private int numberOfStudents = 30;
- private float[] marks = new float[numberOfStudents]; //here we create a marks array of length 30 which is mentioned in the question. Required for mark
+ 
  public static void main (String[] args)//shows the code where to start
  {
+     
+ int numberOfStudents = 30;
+ float[] marks = new float[numberOfStudents]; //here we create a marks array of length 30 which is mentioned in the question. Required for mark
+ Scanner takeinput = new Scanner(System.in);
+     
+     
      System.out.println("Give marks for 30 students: ");
      
      for(int i=0; i< numberOfStudents; i++) {
-         marks[i] = scanner.nextFloat();
+         marks[i] = Scanner.nextFloat();
      
  }
  
