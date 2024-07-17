4a890d9217f39048312a97c026d47cf4402f17bb
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-07-17 22:01:58 +1000

    mean and deviance

--- jovin-copy/studentmark.java
+++ jovin-copy/studentmark.java
@@ -12,25 +12,61 @@ public class studentmark
 {
  
  
- public static void main (String[] args)//shows the code where to start
- {
-     
- int numberOfStudents = 30;
- float[] marks = new float[numberOfStudents]; //here we create a marks array of length 30 which is mentioned in the question. Required for mark
- Scanner scanner = new Scanner(System.in);
+     public static void main (String[] args) {//shows the code where to start
+         int numberOfStudents = 30;
+         float[] marks = new float[numberOfStudents]; //here we create a marks array of length 30 which is mentioned in the question. Required for mark
+         Scanner scanner = new Scanner(System.in);
      
      
-     System.out.println("Give marks for 30 students: ");
+        System.out.println("Give marks for 30 students: ");
      
-     for(int i=0; i< numberOfStudents; i++) {
-         marks[i] = scanner.nextFloat();
+        for(int i=0; i< numberOfStudents; i++){
+             // check if input marks is between 0 and 30
+             float temp = scanner.nextFlaot();
+             
+             if ( temp<0 || temp>30) {
+                 System.out.println("Invalid marks, please enter the marks again");
+             }
+             
+             marks[i] = temp;
      
- }
+        }
  
  
- System.out.println("Entered marks: ");
- for(int j=0; j<numberOfStudents; j++) {
-     System.out.println(marks[j]);
- }
- }
+        System.out.println("Entered marks: ");
+        for(int j=0; j<numberOfStudents; j++) {
+             System.out.println(marks[j]);
+     
+            }
+            
+        // mean = sum of all marks/number of marks
+        float total = 0;
+    
+        for (int i=0; i< numberOfStudents;i++) {
+            total = total + marks[i];
+        }
+        
+        float mean = total/numberOfStudents;
+        
+        float summedDiff=0
+        for (int i=0;i<numberOfStudents; i++) {
+            summedDiff = summedDiff+(marks[i]-mean)*(marks[i]-mean);
+        }
+        
+        summedDiff = summedDiff/numberOfStudents;
+        
+        //float squareRootSummmedDiff = take a power (exponent) of 0.5
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    } 
 } 
